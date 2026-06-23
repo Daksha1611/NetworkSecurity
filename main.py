ee28bf92aa896651ec5c0ab44bf0125e9eba5674
Daksha1611 <mehtadaksha1611@gmail.com>
2026-06-23 23:14:43 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
-
+from networksecurity.components.data_validation import DataValidation
 
 from networksecurity.Exception.Exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.Entity.config_entity import DataIngestionConfig
+from networksecurity.Entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.Entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -17,6 +17,13 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
 
         
         
--- networksecurity/Entity/config_entity.py
+++ networksecurity/Entity/config_entity.py
@@ -16,7 +16,6 @@ class TrainingPipelineConfig:
         self.timestamp: str=timestamp
 
 
-
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str=os.path.join(
@@ -34,6 +33,7 @@ class DataIngestionConfig:
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
+
 
 class DataValidationConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
@@ -60,6 +60,7 @@ class DataTransformationConfig:
             training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
         self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
             training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+ 
         
 class ModelTrainerConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
