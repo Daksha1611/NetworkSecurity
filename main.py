bc70aff96c82c6ddca69d5e42fb2ec6c74d62cec
Daksha1611 <mehtadaksha1611@gmail.com>
2026-06-25 10:20:47 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
-
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.Exception.Exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.Entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.Entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.Entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -16,16 +16,21 @@ if __name__=='__main__':
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
-        print(dataingestionartifact)
+        # print(dataingestionartifact)
         
         data_validation_config = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, data_validation_config)
         logging.info("Initiate the data validation")
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
-        print(data_validation_artifact)
+        # print(data_validation_artifact)
 
-        
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the data transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
         
     except Exception as e:
            raise NetworkSecurityException(e,sys)
